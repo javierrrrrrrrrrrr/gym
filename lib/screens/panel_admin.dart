67d1dd0af277f4c98936a1ffaf89a930aa0e73dd
Javier <javierserrano98050602@gmail.com>
2022-02-06 10:51:48 -0500

    terminado la pagina de home

--- lib/screens/panel_admin.dart
+++ lib/screens/panel_admin.dart
@@ -31,63 +31,105 @@ class PanelAdmin extends StatelessWidget {
               ),
             ),
           ),
+          separador(),
           Row(
             children: [
-              Container(
-                decoration: BoxDecoration(
-                  border: Border.all(),
-                  borderRadius: BorderRadius.circular(25),
-                  color: const Color.fromRGBO(45, 49, 146, 1),
-                ),
-                margin: EdgeInsets.only(left: width * 0.05),
-                height: height * 0.3,
-                width: width * 0.4,
+              Carta(
+                width: width,
+                height: height,
+                texto: "Gestion de clientes",
+                imgurl: "assets/add_user.png",
               ),
               SizedBox(
-                width: width * 0.1,
+                width: width * 0.05,
               ),
-              Container(
-                decoration: BoxDecoration(
-                  border: Border.all(),
-                  borderRadius: BorderRadius.circular(25),
-                  color: const Color.fromRGBO(45, 49, 146, 1),
-                ),
-                height: height * 0.3,
-                width: width * 0.4,
-              ),
-              SizedBox(
-                height: height * 0.45,
+              Carta(
+                width: width,
+                height: height,
+                texto: "Gestion de Entrenador",
+                imgurl: "assets/add_user.png",
               ),
             ],
           ),
+          separador(),
           Row(
             children: [
-              Container(
-                decoration: BoxDecoration(
-                  border: Border.all(),
-                  borderRadius: BorderRadius.circular(25),
-                  color: const Color.fromRGBO(45, 49, 146, 1),
-                ),
-                margin: EdgeInsets.only(left: width * 0.05),
-                height: height * 0.3,
-                width: width * 0.4,
+              Carta(
+                width: width,
+                height: height,
+                texto: "Gestion de clientes",
+                imgurl: "assets/supplies.png",
               ),
               SizedBox(
-                width: width * 0.1,
+                width: width * 0.05,
               ),
-              Container(
-                decoration: BoxDecoration(
-                  border: Border.all(),
-                  borderRadius: BorderRadius.circular(30),
-                  color: const Color.fromRGBO(45, 49, 146, 1),
-                ),
-                height: height * 0.3,
-                width: width * 0.4,
+              Carta(
+                width: width,
+                height: height,
+                texto: "Gestion de Entrenador",
+                imgurl: "assets/add_user.png",
               ),
             ],
           ),
         ],
       ),
     );
+  }
+}
+
+Widget separador() {
+  return const SizedBox(
+    height: 45,
+  );
+}
+
+class Carta extends StatelessWidget {
+  const Carta({
+    Key? key,
+    required this.imgurl,
+    required this.texto,
+    required this.width,
+    required this.height,
+  }) : super(key: key);
+
+  final double width;
+  final double height;
+  final String texto;
+  final String imgurl;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        decoration: BoxDecoration(
+          border: Border.all(),
+          borderRadius: BorderRadius.circular(25),
+          color: const Color.fromRGBO(45, 49, 146, 1),
+        ),
+        margin: EdgeInsets.only(left: width * 0.05),
+        height: height * 0.3,
+        width: width * 0.4,
+        child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.only(top: height * 0.035),
+              height: 120,
+              width: 120,
+              child: Image(
+                image: AssetImage(imgurl),
+                color: Colors.white,
+                fit: BoxFit.cover,
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: height * 0.03, top: height * 0.01),
+              child: Text(
+                texto,
+                style: const TextStyle(color: Colors.white),
+                textScaleFactor: 1.5,
+                textAlign: TextAlign.start,
+              ),
+            ),
+          ],
+        ));
   }
 }
