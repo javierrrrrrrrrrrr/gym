c402a4e8f958329b826ce5012093b81dd23c732a
javierserrano98050602@gmail.com <javierserrano98050602@gmail.com>
2022-02-28 14:51:29 -0500

    borrare lista de usuario al entrar a gestion de entrenadores

--- lib/screens/trainers.dart
+++ lib/screens/trainers.dart
@@ -25,7 +25,6 @@ class _TrainerPageState extends State<TrainerPage> {
             height: height,
             width: width,
           ),
-          ListViewPage(height: height)
         ],
       ),
     );
