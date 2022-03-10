361ec46d2b6ac2d395ded9598453ffee1948a941
javierserrano98050602@gmail.com <javierserrano98050602@gmail.com>
2022-03-09 22:40:41 -0500

    Cambios al cargar imagen al editar

--- lib/widgets/edti_user_img.dart
+++ lib/widgets/edti_user_img.dart
@@ -43,18 +43,24 @@ class _EditUserImgState extends State<EditUserImg> {
                       child: Container(
                     height: height * 0.25,
                     width: width * 0.4,
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(width * 0.025),
-                      child: FadeInImage(
-                        fit: BoxFit.cover,
-                        placeholder: const AssetImage(
-                            'assets/JovialMeagerBull-size_restricted.gif'),
-                        image: NetworkImage((user.img != "no-avatar.png")
-                            ? 'http://78.108.216.56:3000/api/uploads/clients/${user.id}'
-                            : 'https://media.istockphoto.com/vectors/no-image-available-sign-vector-id922962354?k=20&m=922962354&s=612x612&w=0&h=f-9tPXlFXtz9vg_-WonCXKCdBuPUevOBkp3DQ-i0xqo='),
-                        placeholderFit: BoxFit.cover,
-                      ),
-                    ),
+                    child: (user.img != "no-avatar.png")
+                        ? ClipRRect(
+                            borderRadius: BorderRadius.circular(width * 0.025),
+                            child: FadeInImage(
+                              fit: BoxFit.cover,
+                              placeholder:
+                                  const AssetImage('assets/images.jpg'),
+                              image: NetworkImage(
+                                  'http://78.108.216.56:3000/api/uploads/clients/${user.id}'),
+                              placeholderFit: BoxFit.cover,
+                            ),
+                          )
+                        : Container(
+                            decoration: const BoxDecoration(
+                                image: DecorationImage(
+                                    image: AssetImage('assets/images.jpg'),
+                                    fit: BoxFit.cover)),
+                          ),
                     decoration: BoxDecoration(
                         color: const Color.fromRGBO(196, 196, 196, 1),
                         borderRadius: BorderRadius.circular(width * 0.025)),
