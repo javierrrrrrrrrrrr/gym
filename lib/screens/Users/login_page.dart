95033606aef22d0e39304f2ee68a80fb42875617
javierserrano98050602@gmail.com <javierserrano98050602@gmail.com>
2022-03-15 21:25:09 -0400

    ss

--- lib/screens/Users/login_page.dart
+++ lib/screens/Users/login_page.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: non_constant_identifier_names
+// ignore_for_file: non_constant_identifier_names, unused_field
 
 import 'package:flutter/material.dart';
 import 'package:elegant_notification/resources/arrays.dart';
