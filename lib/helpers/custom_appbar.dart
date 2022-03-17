94f2232ef98528801aad50a442518e2c99c91358
Roger Luis <rogerordaz98@gmail.com>
2022-03-17 10:35:18 -0400

    listo lo de observaciones

--- lib/helpers/custom_appbar.dart
+++ lib/helpers/custom_appbar.dart
@@ -4,10 +4,11 @@ AppBar customAppbar(BuildContext context,
     {required double width,
     List<Widget>? actions,
     required String title,
+    bool? centerTitle,
     Widget? leading}) {
   return AppBar(
     leading: leading,
-    centerTitle: true,
+    centerTitle: centerTitle,
     actions: actions,
     leadingWidth: 50,
     backgroundColor: const Color.fromRGBO(77, 82, 233, 1),
