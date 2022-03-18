851fc987fb78fbeb22f433a85b40aebc78a96c1c
Roger Luis <rogerordaz98@gmail.com>
2022-03-17 22:16:56 -0400

    xd

---
+++ lib/widgets/listview_user_body.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+import 'package:gym/models/models.dart';
+import 'package:gym/providers/providers.dart';
+import 'package:provider/provider.dart';
+
+class ListViewUserBody extends StatelessWidget {
+  const ListViewUserBody({
+    Key? key,
+    required this.user,
+  }) : super(key: key);
+
+  final User user;
+
+  @override
+  Widget build(BuildContext context) {
+    final userProvider = Provider.of<UsersProvider>(context);
+    final userFormController = Provider.of<UserFormController>(context);
+    final width = MediaQuery.of(context).size.width;
+    final trainerProvider = Provider.of<TrainerProvider>(context);
+
+    return GestureDetector(
+      onTap: () async {
+        userProvider.selectedUser = user.copyWith();
+        userFormController.user = user.copyWith();
+
+        trainerProvider.getTrainers();
+        Navigator.pushNamed(context, 'edit_user');
+      },
+      child: SizedBox(
+        height: width * 0.19,
+        width: double.infinity,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Padding(
+              padding: EdgeInsets.only(left: width * 0.025),
+              child: Stack(
+                children: [
+                  (user.img != "no-avatar.png")
+                      ? Hero(
+                          tag: user.id,
+                          child: CircleAvatar(
+                            radius: width * 0.065,
+                            backgroundImage:
+                                const AssetImage('assets/images.jpg'),
+                            foregroundImage: NetworkImage(
+                                'http://181.225.253.122:3000/api/uploads/clients/${user.id}'),
+                          ),
+                        )
+                      : Hero(
+                          tag: user.id,
+                          child: CircleAvatar(
+                            radius: width * 0.065,
+                            backgroundImage:
+                                const AssetImage('assets/images.jpg'),
+                          ),
+                        ),
+                  Positioned(
+                    right: 0,
+                    bottom: width * 0.012,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(width * 0.080),
+                      child: Container(
+                        height: width * 0.025,
+                        width: width * 0.025,
+                        color: user.active == true ? Colors.green : Colors.red,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.only(left: width * 0.04),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "${user.firstname} ${user.lastname}",
+                      style: TextStyle(fontSize: width * 0.05),
+                    ),
+                    Row(
+                      children: [
+                        const Icon(Icons.phone),
+                        Text(
+                          user.phone,
+                          style: TextStyle(fontSize: width * 0.04),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(right: width * 0.04),
+              child: IconButton(
+                onPressed: () async {
+                  final trainerProvaider =
+                      Provider.of<TrainerProvider>(context, listen: false);
+                  userProvider.selectedUser = user.copyWith();
+                  userFormController.user = user.copyWith();
+
+                  await trainerProvaider.getTrainers().whenComplete(
+                      () => Navigator.pushNamed(context, 'edit_user'));
+                },
+                icon: Icon(Icons.edit, size: width * 0.09),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
