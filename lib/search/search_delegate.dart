2edfb444dc4f7d2dc3dbc9ca603b73ab6c5e9a6f
Roger Luis <rogerordaz98@gmail.com>
2022-03-17 22:16:51 -0400

    search hecho

--- lib/search/search_delegate.dart
+++ lib/search/search_delegate.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:gym/providers/Users/users_provider.dart';
+import 'package:gym/widgets/listview_user_body.dart';
 import 'package:provider/provider.dart';
-
-import '../models/Users/getUsersModel.dart';
 
 class UserSerch extends SearchDelegate {
   @override
@@ -27,7 +26,7 @@ class UserSerch extends SearchDelegate {
 
   @override
   Widget buildResults(BuildContext context) {
-    return const Text('Result');
+    return const Text('');
   }
 
   @override
@@ -54,80 +53,10 @@ class UserSerch extends SearchDelegate {
 
         return ListView.builder(
             itemCount: user.length,
-            itemBuilder: (_, index) => ListUserBody(user: user[index]));
+            itemBuilder: (_, index) => ListViewUserBody(user: user[index]));
       },
 
       //  padding: const EdgeInsets.only(bottom: 10),
-    );
-  }
-}
-
-class ListUserBody extends StatelessWidget {
-  const ListUserBody({
-    Key? key,
-    required this.user,
-  }) : super(key: key);
-
-  final User user;
-
-  @override
-  Widget build(BuildContext context) {
-    final userProvider = Provider.of<UsersProvider>(context);
-
-    return SizedBox(
-      height: 75,
-      width: double.infinity,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(left: 10),
-            child: CircleAvatar(
-              radius: 25,
-              backgroundImage: const AssetImage(
-                  'assets/JovialMeagerBull-size_restricted.gif'),
-              foregroundImage: NetworkImage((user.img != "no-avatar.png")
-                  ? 'http://181.225.253.122/api/uploads/clients/${user.id}'
-                  : 'https://media.istockphoto.com/vectors/no-image-available-sign-vector-id922962354?k=20&m=922962354&s=612x612&w=0&h=f-9tPXlFXtz9vg_-WonCXKCdBuPUevOBkp3DQ-i0xqo='),
-            ),
-          ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.only(left: 15),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "${user.firstname} ${user.lastname}",
-                    style: const TextStyle(fontSize: 20),
-                  ),
-                  Row(
-                    children: [
-                      const Icon(Icons.phone),
-                      Text(
-                        user.phone,
-                        style: const TextStyle(fontSize: 16),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 15),
-            child: IconButton(
-              onPressed: () {
-                userProvider.selectedUser = user;
-
-                Navigator.pushNamed(context, 'edit_user');
-              },
-              icon: const Icon(Icons.edit, size: 35),
-            ),
-          ),
-        ],
-      ),
     );
   }
 }
